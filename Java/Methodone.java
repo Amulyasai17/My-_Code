304ff95ef5a697df6d6dcef5bf8a5216e57bb287
Amulyasai17 <31813899+Amulyasai17@users.noreply.github.com>
2018-06-21 13:44:34 +0530

    Addition of two numbers without parameters and without return

---
+++ Java/Methodone.java
@@ -0,0 +1,15 @@
+class Methodone
+{
+  public void add()
+{
+   int a=10;
+   int b=20;
+   int c=a+b;
+   System.out.println("C="+c);
+}
+   public static void main(String args[])
+{
+   Methodone ob = new Methodone();
+   ob.add();
+}
+}
